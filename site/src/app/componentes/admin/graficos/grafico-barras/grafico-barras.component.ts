02c241a4f4b232988a02ec458e4f8b8bbe855404
marcosmjr <marcos.moreira.jr@gmail.com>
2026-04-10 21:15:39 -0300

    Contador de Cidades - pastas dist e api

--- site/src/app/componentes/admin/graficos/grafico-barras/grafico-barras.component.ts
+++ site/src/app/componentes/admin/graficos/grafico-barras/grafico-barras.component.ts
@@ -25,7 +25,9 @@ export class GraficoLinhasComponent implements OnInit {
    @ViewChild("estatistica4", { static: true }) elemento4!: ElementRef;
   chart4: any;
 
-   mes: string = "mês";
+  @ViewChild("estatistica5", { static: true }) elemento5!: ElementRef;
+  chart5: any;
+  mes: string = "";
   servico: string = "";
   private vendas: number = 0;
   private manutencao: number = 0;
@@ -35,11 +37,23 @@ export class GraficoLinhasComponent implements OnInit {
   private pessoaFisica: number = 0;
   private pessoaJuridica: number = 0;
 
-  private preferencia: string = "";
-  private telefone: number = 0;
-  private whatsApp: number = 0;
-  private email: number = 0;
+  private preferenciaJuridico: string = "";
+  private telefoneJuridico: number = 0;
+  private whatsAppJuridico: number = 0;
+  private emailjuridico: number = 0;
+  private cidadeJuridica: any;
+  private bairroJuridica:Array<string> = [];
 
+  private preferenciaFisico: string = "";
+  private telefoneFisico: number = 0;
+  private whatsAppFisico: number = 0;
+  private emailFisico: number = 0;
+  private cidadeFisica: Array<string> = [];
+  private bairroFisica: Array<string> = [];
+
+  private cidadesPessoaJuridica: any;
+
+private teste: any;
 
   constructor(private requisicoesService: RequisicoesService){}
 
@@ -96,8 +110,8 @@ export class GraficoLinhasComponent implements OnInit {
         data: {
           labels: ['Telefone', 'WhatsApp', 'E-mail'],
           datasets: [{
-            label: 'Meio e comunicação preferido',
-            data: [this.telefone, this.whatsApp, this.email],
+            label: 'Meio e comunicação preferido - Cliente jurídico',
+            data: [this.telefoneJuridico, this.whatsAppJuridico, this.emailjuridico],
             backgroundColor: ['#42A5F5', '#FFA726', '#26A69A', '#EC407A'],
             borderWidth: 1
           }]
@@ -115,11 +129,11 @@ export class GraficoLinhasComponent implements OnInit {
       this.chart4 = new Chart(this.elemento4.nativeElement, {
         type: 'bar', // Tipo: bar, line, pie, doughnut, etc.
         data: {
-          labels: ['Pessoa física', 'Pessoa juridica'],
+          labels: ['Telefone', 'WhatsApp', 'E-mail'],
           datasets: [{
-            label: 'Seviços 2026',
-            data: [this.pessoaFisica, this.pessoaJuridica],
-            backgroundColor: ['#42A5F5', '#FFA726'],
+            label: 'Meio e comunicação preferido - Cliente físico',
+            data: [this.telefoneFisico, this.whatsAppFisico, this.emailFisico],
+            backgroundColor: ['#42A5F5', '#FFA726', '#26A69A', '#EC407A'],
             borderWidth: 1
           }]
         },
@@ -132,6 +146,26 @@ export class GraficoLinhasComponent implements OnInit {
       });
   }
 
+   renderizarGrafico5() {
+  //     this.chart5 = new Chart(this.elemento5.nativeElement, {
+  //       type: 'bar', // Tipo: bar, line, pie, doughnut, etc.
+  //       data: {
+  //         labels: ['Pessoa física', 'Pessoa juridica'],
+  //         datasets: [{
+  //           label: 'Cliente pessoa físico / Clente pessoa jurídica',
+  //           data: [this.pessoaFisica, this.pessoaJuridica],
+  //           backgroundColor: ['#42A5F5', '#FFA726'],
+  //           borderWidth: 1
+  //         }]
+  //       },
+  //       options: {
+  //         responsive: true,
+  //         plugins: {
+  //           legend: { position: 'top' }
+  //         }
+  //       }
+  //     });
+   }
 
 
   fazerRequisicao(){
@@ -142,11 +176,15 @@ export class GraficoLinhasComponent implements OnInit {
 
         this.contadorServicos(dados);
         this.contadorTipoCliente(dados);
-        this.contadorMeiosComunicacao(dados);
+        this.contadorMeiosComunicacaoJuridica(dados);
+        this.contadorMeiosComunicacaoFisica(dados);
         this.renderizarGrafico1();
         this.renderizarGrafico2();
         this.renderizarGrafico3();
         this.renderizarGrafico4();
+        this.renderizarGrafico5();
+
+        console.log("cidadeJuridica = ", this.cidadeJuridica);
       },
 
       error: (erro) => {
@@ -189,21 +227,54 @@ export class GraficoLinhasComponent implements OnInit {
     }
   }
 
-contadorMeiosComunicacao(dados: RespostaAPI){
+contadorMeiosComunicacaoJuridica(dados: RespostaAPI){
    for(var i = 0; i < dados.data.length; i++){
-        this.preferencia = dados.data[i]['preferencia_cliente_juridico'] != null ? dados.data[i]['preferencia_cliente_juridico'] : "";
+        this.preferenciaJuridico = dados.data[i]['preferencia_cliente_juridico'] != null ? dados.data[i]['preferencia_cliente_juridico'] : "";
 
-        if(this.preferencia[0] == "t" || this.preferencia[1] == "t" || this.preferencia[2] == "t" ){
-          this.telefone += 1;
+        if(this.preferenciaJuridico[0] == "t" || this.preferenciaJuridico[1] == "t" || this.preferenciaJuridico[2] == "t" ){
+          this.telefoneJuridico += 1;
         }
-        if(this.preferencia[0] == "w" || this.preferencia[1] == "w" || this.preferencia[2] == "w"){
-          this.whatsApp += 1;
+        if(this.preferenciaJuridico[0] == "w" || this.preferenciaJuridico[1] == "w" || this.preferenciaJuridico[2] == "w"){
+          this.whatsAppJuridico += 1;
         }
-        if(this.preferencia[0] == "e" || this.preferencia[1] == "e" || this.preferencia[2] == "e"){
-          this.email += 1;
+        if(this.preferenciaJuridico[0] == "e" || this.preferenciaJuridico[1] == "e" || this.preferenciaJuridico[2] == "e"){
+          this.emailjuridico += 1;
+        }
+   }
+}
+
+contadorMeiosComunicacaoFisica(dados: RespostaAPI){
+   for(var i = 0; i < dados.data.length; i++){
+
+        this.preferenciaFisico = dados.data[i]['preferencia_cliente_fisico'] != null ? dados.data[i]['preferencia_cliente_fisico'] : "";
+
+        if(this.preferenciaFisico[0] == "t" || this.preferenciaFisico[1] == "t" || this.preferenciaFisico[2] == "t" ){
+          this.telefoneFisico += 1;
+        }
+        if(this.preferenciaFisico[0] == "w" || this.preferenciaFisico[1] == "w" || this.preferenciaFisico[2] == "w"){
+          this.whatsAppFisico += 1;
+        }
+        if(this.preferenciaFisico[0] == "e" || this.preferenciaFisico[1] == "e" || this.preferenciaFisico[2] == "e"){
+          this.emailFisico += 1;
         }
 
    }
+}
+
+contadorCidadesJuridica(dados: RespostaAPI): void{
+  let cidades: string[] = [];
+ for(let i in dados.data){
+  if(dados.data[i].cidade_cliente_juridico != undefined){
+    cidades.push(dados.data[i].cidade_cliente_juridico);
+  }
+ }
+
+  const contagem = cidades.reduce((acc: Record<string, number>, cidade: any) => {
+    acc[cidade] = (acc[cidade] || 0) + 1;
+    const resultado = [acc];
+    this.cidadeJuridica = acc;
+    return acc;
+  }, {});
 }
 
 /*************************************************** */
